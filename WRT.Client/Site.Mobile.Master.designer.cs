f6f48461e85c52bc162d1563cff31a70bf0174bc
Richard Segerlund <richard.segerlund@consid.se>
2018-06-08 11:37:03 +0200

    Added buttons in mobile

--- WRT.Client/Site.Mobile.Master.designer.cs
+++ WRT.Client/Site.Mobile.Master.designer.cs
@@ -31,6 +31,24 @@ namespace WRT.Client {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// btnSaveNewCompetitor control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSaveNewCompetitor;
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
         /// FeaturedContent control.
         /// </summary>
         /// <remarks>
